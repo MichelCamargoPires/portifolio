2c012d1b2f51ac036ce9a436ac66f3ff687ae7fe
MichelCamargoPires <95002779+MichelCamargoPires@users.noreply.github.com>
2022-02-20 15:06:23 -0300

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+document.querySelector(".hamburguer").addEventListener("click",() =>{
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
